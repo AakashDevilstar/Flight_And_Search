89022e28dbda8256bd460aa7f542f48d3d9c661c
AakashDevilstar <akashraturi2001@gmail.com>
2023-04-14 00:00:11 +0530

    Setup Basic Bodyparser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const {PORT}=require('../src/config/Serverconfig');
+const bodyParser = require('body-parser');
 
 const setupserver=async()=>{
     const app=express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
         // console.log(process.env);
